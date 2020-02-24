3274f9540c3497859451c3b090ba9930b2f2a36f
zhengwu.li <641876684@qq.com>
2020-02-24 19:50:34 +0800

    init

--- product-springboot/src/main/java/com/lzw/config/SpringWebMvcConfiguration.java
+++ product-springboot/src/main/java/com/lzw/config/SpringWebMvcConfiguration.java
@@ -7,11 +7,7 @@
  */
 package com.lzw.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,11 +21,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class SpringWebMvcConfiguration implements WebMvcConfigurer {
-
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new MappingJackson2CborHttpMessageConverter());
-	}
-
 
 }
